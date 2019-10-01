df3710661256402c5de7a852cbaef4957ebf0a1a
tmarquez1497 <marquezt@knights.ucf.edu>
2019-10-01 14:45:21 -0400

    In progress (File Processing)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Zork
 {
     class Program
     {
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
+        }
 
         private static Room CurrentRoom
         {
@@ -19,7 +28,11 @@ namespace Zork
         //Main Function
         static void Main(string[] args)
         {
-           InitializeRoomDescription();
+            //string roomsFilename = "Rooms.txt";
+            //InitializeRoomDescription(roomsFilename);
+          const string defaultRoomsFilename = "Rooms.txt";
+
+           string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
            Console.WriteLine("Welcome to Zork!");
 
             Room previousRoom = null;
@@ -122,27 +135,39 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
+        //private static readonly Dictionary<string, Room> RoomMap;
 
-        private static void InitializeRoomDescription()
+        private static readonly Dictionary<string, Room> RoomMap;
+
+        private enum Fields
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        private static void InitializeRoomDescription(string roomsFilename)
+        {
+            const string fieldDelimeter = "##";
+            const int expectedFieldCount = 2;
+
+            var roomQuery = from line in File.ReadAllLines(roomsFilename)
+                            let fields = line.Split(fieldDelimeter)
+                            where fields.Length == expectedFieldCount
+                            select (Name: fields[(int)Fields.Name],
+                                    Description: fields[(int)Fields.Description]);
+
+            foreach(var (Name, Description) in roomQuery)
             {
-                roomMap[room.Name] = room;
+                RoomMap[Name].Description = Description;
             }
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail."; //Rocky Trail
-            roomMap["South of House"].Description = "You are facing the south side of a white house.There is no door here, and all the windows are barred."; // South of House
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall."; //Canyon View
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you."; //Forest
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door."; //West of White House
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; //Behind House
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of House
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
-
-
+            
+            
+         
         }
 
 
